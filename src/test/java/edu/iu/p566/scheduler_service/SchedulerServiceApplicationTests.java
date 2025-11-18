b25339cbb8d2e3d228ad4f6718cd651f95dcb339
Veda <619veda@gmail.com>
2025-11-18 17:17:21 -0500

    Initial Commit for Scheduler_Service

---
+++ src/test/java/edu/iu/p566/scheduler_service/SchedulerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.p566.scheduler_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
